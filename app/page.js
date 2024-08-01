3fd3a735e82e37bc4ad50626d3af38f8634596aa
Anthony Silva <silvanth@oregonstate.edu>
2024-07-31 20:05:36 -0700

    Pulled from another repository with security issues, and fixed them.

---
+++ app/page.js
@@ -0,0 +1,201 @@
+'use client'
+import { Typography, Button, Modal, TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { firestore } from '@/firebase';
+import { collection, doc, getDocs, getDoc, query, setDoc, deleteDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'white',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 3,
+};
+
+export default function Home() {
+  const [pantry, setPantry] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [itemName, setItemName] = useState('');
+
+  const updatePantry = async () => {
+    const snapshot = query(collection(firestore, 'pantry'));
+    const docs = await getDocs(snapshot);
+    const pantryList = [];
+    docs.forEach((doc) => {
+      pantryList.push({"name": doc.id, "count": doc.data().count});
+    })
+    console.log(pantryList);
+    setPantry(pantryList);
+  }
+
+  useEffect( () => {
+      
+    updatePantry()
+  }, [])
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    // Check if exists
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const {count} = docSnap.data()
+      await setDoc(docRef, {count: count + 1})
+    } else {
+        await setDoc(docRef, {count: 1})
+    }
+    await updatePantry()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const {count} = docSnap.data()
+      if(count === 1){
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, {count: count - 1})
+      }
+      
+    }
+    await updatePantry()
+  }
+  return (
+    <Box 
+    width="100vw" 
+    height="100vh" 
+    display={"flex"} 
+    justifyContent={"center"} 
+    flexDirection={'column'}
+    alignItems={"center"}
+    gap={2}
+    >
+      <Box
+      border={'1px solid #333'}
+      >
+
+      <Box
+      width='800px'
+      height='100px'
+      bgcolor={'#ADD8E6'}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      >
+        <Typography
+        variant={'h2'}
+        color={'#333'}
+        textAlign={'center'}
+        >
+          Pantry Items
+        </Typography>
+      </Box>
+      <Stack
+      width="800px"
+      height="300px"
+      spacing={2}
+      overflow={"auto"}
+      >
+        {pantry.map(({name, count}) => (
+          //<Stack
+          //key={i}
+          //direction={'row'}
+          //spacing={2}
+          //justifyContent={'center'}
+          //alignContent={'space-between'}
+          //>
+            <Box
+            key={name}
+            width="100%"
+            minHeight="150px"
+            display={"flex"}
+            justifyContent={'space-between'}
+            alignItems={'space-between'}
+            bgcolor={'#f0f0f0'}
+            paddingX={5}
+            >
+              <Typography
+              variant={'h3'}
+              color={'#333'}
+              textAlign={'center'}
+              >
+                {
+                  // Capitalize the first letter of the item
+                  name.charAt(0).toUpperCase() + name.slice(1)
+                }
+              </Typography>
+              <Typography
+                variant={'h3'}
+                color={'#333'}
+                textAlign={'center'}
+              >
+                Quantity: {count}
+              </Typography>
+            <Button
+            variant="contained"
+            onClick={ () =>{
+              removeItem(name)
+              setItemName('')
+              handleClose()
+            }}
+            >
+              Remove
+            </Button>
+            </Box>
+          //</Stack>
+        ))}
+      </Stack>
+      </Box>
+      <Button variant="contained" onClick={handleOpen}>Add</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack
+          width="100%"
+          direction={'row'}
+          spacing={2}
+          >
+            <TextField
+            id="outlined-basic"
+            label="Item"
+            variant="outlined"
+            fullWidth
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+            variant="outlined"
+            onClick={ () =>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>    
+    </Box>
+  );
+}
