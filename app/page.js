9ebbcd2c790f16c738c62d554c294656be7a83ab
Anthony Silva <silvanth@oregonstate.edu>
2024-08-04 01:09:04 -0700

    Installed OpenAI for future usage, and updated CRUD functionalities

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Stack from '@mui/material/Stack';
 import { firestore } from '@/firebase';
 import { collection, doc, getDocs, getDoc, query, setDoc, deleteDoc } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+// import { OpenAI } from 'openai'
 
 const style = {
   position: 'absolute',
@@ -21,22 +22,49 @@ const style = {
   gap: 3,
 };
 
+// const openAI = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPEN_AI_API_KEY });
+
+/*const getRecipeSuggestions = async (pantry) => {
+  const pantryItems = pantry.map(({ name, count }) => `${name}: ${count}`).join(", ");
+  const completion = await openAI.chat.completions.create({
+    messages: [{ role: "system", content: `Given the following pantry items: ${pantryItems}, suggest some recipes.` }],
+    model: "gpt-4o-mini",
+  });
+
+  return (completion.data.choices[0].text);
+}*/
+
 export default function Home() {
   const [pantry, setPantry] = useState([]);
-
-  const [openAddModal, setOpenAddModal] = useState(false);
+  const [removeItemName, setRemoveItemName] = useState('');
+  const [removeQuantity, setRemoveQuantity] = useState(1);
+  const [addItemName, setAddItemName] = useState('');
+  const [addQuantity, setAddQuantity] = useState(1);
+  const [itemName, setItemName] = useState('');
+  const [openNewModal, setOpenNewModal] = useState(false);
   const [openRemoveModal, setOpenRemoveModal] = useState(false);
-  const handleOpenAddModal = () => setOpenAddModal(true);
+  const [openAddModal, setOpenAddModal] = useState(false)
+  const handleOpenNewModal = () => setOpenNewModal(true);
   const [itemQuantity, setItemQuantity] = useState(1);
-  const handleCloseAddModal = () => setOpenAddModal(false);
+  //const [openRecipeModal, setOpenRecipeModal] = useState(false);
+  //const [recipeSuggestions, setRecipeSuggestions] = useState('');
+  const handleCloseNewModal = () => setOpenNewModal(false);
   const handleOpenRemoveModal = (name) => {
     setRemoveItemName(name);
     setOpenRemoveModal(true);
   };
   const handleCloseRemoveModal = () => setOpenRemoveModal(false);
-  const [removeItemName, setRemoveItemName] = useState('');
-  const [removeQuantity, setRemoveQuantity] = useState(1);
-  const [itemName, setItemName] = useState('');
+  const handleOpenAddModal = (name) => {
+    setAddItemName(name);
+    setOpenAddModal(true);
+  }
+  const handleCloseAddModal = () => setOpenAddModal(false);
+  /*const handleOpenRecipeModal = async () => {
+    const suggestions = await getRecipeSuggestions(pantry);
+    setRecipeSuggestions(suggestions);
+    setOpenRecipeModal(true);
+  };
+  const handleCloseRecipeModal = () => setOpenRecipeModal(false);*/
 
   const updatePantry = async () => {
     const snapshot = query(collection(firestore, 'pantry'));
@@ -53,7 +81,7 @@ export default function Home() {
     updatePantry()
   }, [])
 
-  const addItem = async (item, quantity) => {
+  const newItem = async (item, quantity) => {
     const docRef = doc(collection(firestore, 'pantry'), item)
     // Check if exists
     const docSnap = await getDoc(docRef)
@@ -71,13 +99,27 @@ export default function Home() {
     const docSnap = await getDoc(docRef)
     if (docSnap.exists()) {
       const {count} = docSnap.data()
-      await setDoc(docRef, {count: count - quantity})
-      if(count === 0){
+      const newCount = count - quantity
+      if(newCount <= 0){
         await deleteDoc(docRef)
+      }
+      else {
+        await setDoc(docRef, {count: count - quantity})
       }
     }
     await updatePantry()
   }
+
+  const addItem = async (item, quantity) => {
+    const docRef = doc(collection(firestore, 'pantry'), item)
+    const docSnap = await getDoc(docRef)
+    if(docSnap.exists()) {
+      const {count} = docSnap.data()
+      await setDoc(docRef, {count: count + quantity})
+    }
+    await updatePantry()
+  }
+
   return (
     <Box 
     width="100vw" 
@@ -147,8 +189,16 @@ export default function Home() {
                 color={'#333'}
                 textAlign={'center'}
               >
-                Quantity: {count}
+                {count}
               </Typography>
+            <Button
+            variant="contained"
+            onClick={ () => {
+              handleOpenAddModal(name)
+            }}
+            >
+              Add
+            </Button>
             <Button
             variant="contained"
             onClick={ () =>{
@@ -162,16 +212,16 @@ export default function Home() {
         ))}
       </Stack>
       </Box>
-      <Button variant="contained" onClick={handleOpenAddModal}>Add</Button>
+      <Button variant="contained" onClick={handleOpenNewModal}>New Item</Button>
       <Modal
-        open={openAddModal}
-        onClose={handleCloseAddModal}
+        open={openNewModal}
+        onClose={handleCloseNewModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Add Item
+            New Item
           </Typography>
           <Stack
           width="100%"
@@ -198,14 +248,21 @@ export default function Home() {
             <Button
             variant="outlined"
             onClick={ () =>{
-              addItem(itemName, itemQuantity)
+              newItem(itemName, itemQuantity)
               setItemName('')
               setItemQuantity(1)
-              handleCloseAddModal()
+              handleCloseNewModal()
             }}
             >
               Add
             </Button>
+            {/*
+            <Button
+            variant="contained"
+            onClick={handleOpenRecipeModal}
+            >
+              Get Recipes
+            </Button>*/}
           </Stack>
         </Box>
       </Modal>
@@ -226,7 +283,7 @@ export default function Home() {
           >
             <TextField
             id="outlined-basic"
-            label="Item"
+            label="Number To Remove"
             variant="outlined"
             type="number"
             fullWidth
@@ -245,7 +302,66 @@ export default function Home() {
             </Button>
           </Stack>
         </Box>
-      </Modal>       
+      </Modal>
+      <Modal
+        open={openAddModal}
+        onClose={handleCloseAddModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack
+          width="100%"
+          direction={'row'}
+          spacing={2}
+          >
+            <TextField
+            id="outlined-basic"
+            label="Number to Add"
+            variant="outlined"
+            type="number"
+            fullWidth
+            value={addQuantity}
+            onChange={(e) => setAddQuantity(parseInt(e.target.value))}
+            />
+            <Button
+            variant="outlined"
+            onClick={ () =>{
+              addItem(addItemName, addQuantity)
+              setAddQuantity(1)
+              handleCloseAddModal()
+            }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
+      {/*
+      <Modal
+        open={openRecipeModal}
+        onClose={handleCloseRecipeModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Recipe Suggestions
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {recipeSuggestions}
+          </Typography>
+          <Button
+            variant="outlined"
+            onClick={handleCloseRecipeModal}
+          >
+            Close
+          </Button>
+        </Box>
+      </Modal>*/}     
     </Box>
   );
 }
